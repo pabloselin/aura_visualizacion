cf81e0eb009bc01a76b259f85eb099d96024a9ee
Pablo Selín Carrasco Armijo <pabloselin@gmail.com>
2018-12-27 13:28:58 -0300

    ajustes varios y state de node activo

--- src/Graph.js
+++ src/Graph.js
@@ -1,24 +1,34 @@
 import React, { Component } from "react";
 import graphStyle from "./graphStyle";
-import ReactCytoscape from "react-cytoscapejs";
+import CytoscapeComponent from "react-cytoscapejs";
 import popper from "cytoscape-popper";
 
 class Graph extends Component {
 	constructor(props) {
 		super(props);
 		this.state = {
-			activeNode: null
+			activeNode: null,
+			_handleCyCalled: false,
+			layoutChange: false
 		};
 	}
 
-	cyRef(cy) {
-		this.cy = cy;
+	handleCy = cy => {
+		if (cy === this._cy && this.state._handleCyCalled) {
+			return;
+		}
+
+		this._cy = cy;
+		window.cy = cy;
+		this.setState({ _handleCyCalled: true });
+
 		let articulos = cy.elements('node[type="articulo"]');
 		let ediciones = cy.elements('node[type="edicion"]');
 		let terms = cy.elements('node[type="term"]');
 		ediciones.addClass("edicion");
 		articulos.addClass("articulo");
 		terms.addClass("term");
+
 		cy.on("tap", "node", function(evt) {
 			let node = evt.target;
 			node.addClass("active");
@@ -26,15 +36,17 @@ class Graph extends Component {
 				window.location.href = node.data().link;
 			}
 		});
-		cy.on("mouseover", "node", function(evt) {
+
+		cy.on("mouseover", "node", event => {
 			cy.elements("node").removeClass("hover");
-			let node = evt.target;
+			let node = event.target;
+			let nodeid = node.id();
+			this.setState({ activeNode: node.data() });
 			node.addClass("hover");
-			let neighbors = cy
-				.elements("node#" + node.id())
-				.closedNeighborhood();
+			let neighbors = cy.elements("node#" + nodeid).closedNeighborhood();
 			neighbors.addClass("hover");
 		});
+
 		cy.on("taphold", "node", function(evt) {
 			cy.elements("node").removeClass("hover");
 			let node = evt.target;
@@ -50,38 +62,46 @@ class Graph extends Component {
 		cy.on("tapend", "node", function(evt) {
 			cy.elements("node, edge").removeClass("hover");
 		});
-	}
 
-	handleEval() {
-		const cy = this.cy;
-		const str = this.text.value;
-		eval(str);
-	}
+		if (this.state.layoutChange === true) {
+			let layout = cy.layout(this.props.layout);
+			layout.run();
+			this.setState({ layoutChange: false });
+		}
+	};
 
 	componentDidUpdate(prevProps, prevState) {
 		if (this.props.layout !== prevProps.layout) {
 			console.log("layout change");
 		}
 		if (this.props.data !== prevProps.data) {
-			console.log("data change");
-			let layout = this.cy.layout(this.props.layout);
+			this._cy = cy;
+			window.cy = cy;
+			let articulos = cy.elements('node[type="articulo"]');
+			let ediciones = cy.elements('node[type="edicion"]');
+			let terms = cy.elements('node[type="term"]');
+			ediciones.addClass("edicion");
+			articulos.addClass("articulo");
+			terms.addClass("term");
+			let layout = cy.layout(this.props.layout);
 			layout.run();
 		}
 	}
 
 	render() {
 		return (
-			<ReactCytoscape
+			<CytoscapeComponent
 				containerID="cy"
-				elements={ReactCytoscape.normalizeElements(this.props.data)}
-				cy={cy => {
-					this.cyRef(cy);
-				}}
+				elements={CytoscapeComponent.normalizeElements(this.props.data)}
+				cy={this.handleCy}
 				wheelSensitivity={0.2}
 				minZoom={0.7}
 				maxZoom={1.4}
+				zoom={1}
 				layout={this.props.layout}
 				stylesheet={graphStyle}
+				zoomingEnabled={false}
+				userZoomingEnabled={true}
 			/>
 		);
 	}
--- src/layouts.js
+++ src/layouts.js
@@ -6,34 +6,44 @@ const layouts = {
 		fit: true,
 		circle: true,
 		padding: 10,
-		nodeDimensionsIncludeLabels: false
+		nodeDimensionsIncludeLabels: false,
+		animate: true,
+
 	},
 	grid: {
 		name: "grid",
 		fit: true,
 		padding: 10,
-		nodeDimensionsIncludeLabels: false
+		nodeDimensionsIncludeLabels: false,
+		animate: true,
+		zoom: 0.8
 	},
 	breadthfirst: {
 		name: "breadthfirst",
 		fit: true,
 		circle: true,
 		padding: 10,
-		nodeDimensionsIncludeLabels: true
+		nodeDimensionsIncludeLabels: true,
+		animate: true,
+		zoom: 0.8
 	},
 	cose: {
 		name: "cose",
 		fit: true,
 		circle: true,
 		padding: 10,
-		nodeDimensionsIncludeLabels: true
+		nodeDimensionsIncludeLabels: true,
+		animate: true,
+		zoom: 0.8
 	},
 	concentric: {
 		name: "concentric",
 		fit: true,
 		circle: true,
 		padding: 10,
-		nodeDimensionsIncludeLabels: false
+		nodeDimensionsIncludeLabels: false,
+		animate: true,
+		zoom: 0.8
 	}
 };
 
